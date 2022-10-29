0c800ddaa76736a6020b01d59af06f6b620e756c
hayate1121 <112634636+hayate1121@users.noreply.github.com>
2022-10-29 12:48:49 +0000

    initial nagano_cake

---
+++ db/migrate/20221029123231_create_orders.rb
@@ -0,0 +1,17 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      
+      t.integer :customer_id
+      t.integer :payment_method
+      t.integer :status
+      t.integer :shipping_cost
+      t.string :postal_code
+      t.string :address
+      t.string :name
+      t.integer :total_payment
+
+      t.timestamps
+    end
+  end
+end
