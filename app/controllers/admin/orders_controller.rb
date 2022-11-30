29e0eb69a08ed9798c074aec96e9017c6131be68
EC2 Default User <ec2-user@ip-172-31-6-240.ap-northeast-1.compute.internal>
2022-11-30 13:21:52 +0000

    1130nagana

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,14 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @total = 0
+  end
+  
+  def update
+    order = Order.find(params[:id])
+    order.update(order_params)
+    redirect_to admin_order_path(order.id)
   end
   
   private
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,6 @@
 class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
+  
+  enum making_status: {impossible:0, wait:1, production:2, completed:3}
 end
