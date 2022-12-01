786886a3b8121d5f857cfb71aac93bdbdeafd94c
EC2 Default User <ec2-user@ip-172-31-6-240.ap-northeast-1.compute.internal>
2022-12-01 14:25:28 +0000

    1201nagano

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,6 +11,10 @@ class Customer < ApplicationRecord
     self.last_name + self.first_name
   end
   
+  def full_name_kana
+    self.last_name_kana + self.first_name_kana
+  end
+  
   def address_display
     "〒" + self.postal_code + " " + self.address + " " + self.full_name
   end
