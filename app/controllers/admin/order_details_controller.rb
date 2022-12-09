60e1c7fe65515b7cec6bb6770c807635c93bc37a
EC2 Default User <ec2-user@ip-172-31-6-240.ap-northeast-1.compute.internal>
2022-12-09 12:19:16 +0000

    [fin]NaganoCake

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,12 +4,8 @@ class Admin::OrderDetailsController < ApplicationController
     order_detail.update(order_detail_params)
     @order = Order.find(order_detail.order_id)
     @order_details = @order.order_details
-    @order_details.each do |order_detail|
-      if order_detail.making_status != "completed"
-        return
-      else
-        order_detail.order.update(status: "preparing")
-      end
+    if @order_details.where(making_status: "completed").count == @order_details.count
+      order_detail.order.update(status: "preparing")
     end
     redirect_to admin_order_path(order_detail.order.id)
   end
